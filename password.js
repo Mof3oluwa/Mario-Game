92f6b42988a0eb8144edc5e3e1365ef22258073b
Mof3oluwa <152526624+Mof3oluwa@users.noreply.github.com>
2024-10-24 03:49:43 +0100

    Create password.js

---
+++ password.js
@@ -0,0 +1,18 @@
+//This program is written to crash if the password is typed incorrectly three times 
+var password = prompt("Set your password")
+let pin = "Mof3oluwa";
+
+for(i = 0;i<=0;i++){
+  prompt("Confirm your password")
+}
+
+
+let attempts = 3;
+var login = prompt("Login with password")
+for(i = 0;i<=1;i++){
+  alert("Wrong Password!")
+  prompt("Login with password")
+  function stop(){
+  clearTimeout(animate);
+}
+}
